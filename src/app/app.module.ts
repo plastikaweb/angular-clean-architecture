18b028e460963abcdaaf2abaf38820d16cc2d03f
plastikaweb <info@plastikaweb.com>
2021-02-12 12:08:52 +0100

    Upgrade to angular 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
-import { DataModule } from './data/data.module';
 import { CoreModule } from './core/core.module';
 import { ElephantRepository } from './core/repositories/elephant.repository';
+import { DataModule } from './data/data.module';
+import {
+    ElephantMockRepository
+} from './data/repository/elephant-mock-repository/elephant-mock.repository';
 import { PresentationModule } from './presentation/presentation.module';
-import { ElephantMockRepository } from './data/repository/elephant-mock-repository/elephant-mock.repository';
 
 @NgModule({
   declarations: [
